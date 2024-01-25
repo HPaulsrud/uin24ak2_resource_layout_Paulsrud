0c9b97aede22c76877d1cf5696c34638b5cb7d17
HPaulsrud <havard.paulsrud@gmail.com>
2024-01-25 22:52:25 +0100

    Arbeidskrav 2
    ferdig innlevert arbeidskrav 2

---
+++ ressurser.js
@@ -0,0 +1,138 @@
+const resources = [
+    {
+        category: "HTML",
+        text: "HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/html/"
+            },
+            {
+                title: "HTML Living standard",
+                url: "https://html.spec.whatwg.org/multipage/"
+            },
+            {
+                title: "HTML.com Tutorials",
+                url: "https://html.com/"
+            },
+        ]
+    },
+    {
+        category: "CSS",
+        text: "CSS står for Cascading StyleSheets, og brukes for å sette stilregler på HTML-elementer.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/css/"
+            },
+            {
+                title: "W3C HTML & CSS Standards",
+                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
+            },
+            {
+                title: "W3C CSS Validator",
+                url: "https://jigsaw.w3.org/css-validator/"
+            },
+            {
+                title: "CSS Tricks",
+                url: "https://css-tricks.com/"
+            },
+        ]
+    },
+    {
+        category: "JavaScript",
+        text: "JavaScript er et scriptspråk basert på EcmaScript. JavaScript kjører direkte i nettleseren, og brukes ofte til å manipulere HTML og CSS i webgrensnesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/js/"
+            },
+            {
+                title: "MDN Web Docs",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "React",
+        text: "React er et rammeverk bygget i JavaScript. React bruker komponenter og states for å lage en levende frontend.",
+        sources: [
+            {
+                title: "React documentation",
+                url: "https://reactjs.org/docs/getting-started.html"
+            },
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Sanity and headless CMS",
+        text: "Sanity er et headless CMS som står for innholdsadministrasjon. Innhold hentes inn i applikasjoner via GROQ-spørringer.",
+        sources: [
+            {
+                title: "Sanity documentation",
+                url: "https://www.sanity.io/docs"
+            },
+            {
+                title: "OnCrawl: a beginners guide to headless CMS",
+                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
+            },
+            {
+                title: "Section.io: Getting started with Sanity CMS",
+                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
+            },
+        ]
+    },
+]
+
+
+// Create tabs using map
+const tabHeader = document.querySelector('.tab-header');
+resources.map((topic, index) => {
+  const tab = document.createElement('span');
+  tab.className = 'tab';
+  tab.textContent = topic.category;
+  tab.addEventListener('click', () => changeTab(index + 1));
+  tabHeader.appendChild(tab);
+});
+
+// Create tab contents
+const tabContents = document.querySelector('.tab-contents');
+resources.map((topic, index) => {
+  const tabContent = document.createElement('article');
+  tabContent.className = 'tab-content';
+  tabContent.id = 'tab' + (index + 1);
+  tabContent.innerHTML = `<h2>${topic.category}</h2><p>${topic.text}</p><ul>${topic.sources.map(source => `<li><a href="${source.url}" target="_blank">${source.title}</a></li>`).join('')}</ul>`;
+  tabContents.appendChild(tabContent);
+});
+
+
+  // Show default tab
+  changeTab(1);
+
+  function changeTab(tabNumber) {
+    document.querySelectorAll('.tab-content').forEach(content => {
+        content.classList.remove('active');
+      });
+    
+      // Hide all tabs
+      document.querySelectorAll('.tab').forEach(tab => {
+        tab.classList.remove('active');
+      });
+    
+      // Show the selected tab content
+      document.getElementById('tab' + tabNumber).classList.add('active');
+    
+      // Show the selected tab
+      document.querySelector('.tab-header').children[tabNumber - 1].classList.add('active');
+    }
